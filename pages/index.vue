de141366c263d3415f8b732273909364627be3cc
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-04-10 23:01:59 -0300

    estiliazando pagina

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,64 @@
 
     <!-- Search -->
     <div class="container search">
-      <input v-model.lazy="searchInput" type="text" placeholder="Search" @keyup.enter="$fetch" />
-      <button v-show="searchInput !== ''" class="button">Clear Search</button>
+      <input
+        v-model.lazy="searchInput"
+        type="text"
+        placeholder="Search"
+        @keyup.enter="$fetch"
+      />
+      <button v-show="searchInput !== ''" class="button" @click="clearSearch">
+        Clear Search
+      </button>
     </div>
 
+    <!-- Loading -->
+    <loading v-if="$fetchState.pending" />
+
     <!-- Movies -->
-    <div class="container movies">
-      <div id="movie-grid" class="movies-grid">
+    <div v-else class="container movies">
+      <!-- Searched Movies -->
+      <div v-if="searchInput !== ''" id="movie-grid" class="movies-grid">
+        <div
+          v-for="(movie, index) in searchedMovies"
+          :key="index"
+          class="movie"
+        >
+          <div class="movie-img">
+            <img
+              :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+              alt=""
+            />
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.legth > 25">...</span>
+            </p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <nuxt-link
+              class="button button-light"
+              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
+            >
+              Get More Info</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+
+      <!-- Now Streaming -->
+      <div v-else id="movie-grid" class="movies-grid">
         <div v-for="(movie, index) in movies" :key="index" class="movie">
           <div class="movie-img">
             <img
@@ -52,10 +103,11 @@
 <script>
 import axios from 'axios'
 import Hero from '~/components/Hero.vue'
+import Loading from '~/components/Loading.vue'
 
 export default {
   name: 'IndexPage',
-  components: { Hero },
+  components: { Hero, Loading },
 
   data() {
     return {
@@ -65,18 +117,16 @@ export default {
     }
   },
 
-  async fetch(){
+  async fetch() {
     if (this.searchInput === '') {
       await this.getMovies()
     }
 
     if (this.searchInput !== '') {
-      await this.searchedMovies()
+      await this.searchMovies()
     }
   },
-
-  
-
+  fetchDelay: 1200,
   methods: {
     async getMovies() {
       const data = axios.get(
@@ -89,13 +139,20 @@ export default {
       })
     },
 
-    async searchedMovie() {
-      const data = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a866f83e29aa185916154312b86a8e1f&language=en-US&page=1&query=${this.searchInput}`)
+    async searchMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=a866f83e29aa185916154312b86a8e1f&language=en-US&page=1&query=${this.searchInput}`
+      )
 
       const result = await data
       result.data.results.forEach((movie) => {
         this.searchedMovies.push(movie)
       })
+    },
+
+    clearSearch() {
+      this.searchInput = ''
+      this.searchedMovies = []
     },
   },
 }
