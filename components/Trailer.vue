f042c703b76841e2bb1e804a566ceef86951a1a5
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-04-12 08:26:18 -0300

    aplicando responsividade

--- components/Trailer.vue
+++ components/Trailer.vue
@@ -1,59 +1,66 @@
 <template>
   <div>
-        <div v-for="(video, index) in videos" :key="index" class="videos">
-          <div class="container videos">
-            <iframe
-              width="70%"
-              height="600"
-              :src="`https://www.youtube.com/embed/${video.key}`"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            >
-            </iframe>
-          </div>
-        </div>
+    <div v-for="(video, index) in videos" :key="index" class="videos">
+      <div class="container videos">
+        <iframe
+          width="70%"
+          height="600"
+          :src="`https://www.youtube.com/embed/${video.key}`"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        >
+        </iframe>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-    name: 'TrailerVideo',
+  name: 'TrailerVideo',
 
-    data(){
-        return{
-            videos: [],
-        }
-    },
+  data() {
+    return {
+      videos: [],
+    }
+  },
 
-    async fetch() {
+  async fetch() {
     await this.getVideo()
   },
 
   methods: {
-      async getVideo() {
+    async getVideo() {
       const response = await axios.get(
         `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}/videos?api_key=a866f83e29aa185916154312b86a8e1f&language=en-US`
       )
 
-      this.videos = response.data.results;
-
+      this.videos = response.data.results
     },
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-    display: flex;
-    flex-direction: column;
-    margin: 2rem auto;
-    
-    .videos {
-      align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 2rem auto;
+
+  .videos {
+    align-items: center;
+
+    @media screen and (max-width: 500px) {
+    .videos{
+      width: 50%;
+      height: 100px;
+      margin: 2rem auto;
+      }
     }
+  }
 }
 </style>
