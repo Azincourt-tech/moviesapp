31dabd3c2affcde95b78ba8bbb2a7ea4944d7575
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-04-13 16:58:40 -0300

    realizando teste unitario

--- test/Trailer.spec.js
+++ test/Trailer.spec.js
@@ -1,9 +1,27 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import Trailer from '@/components/Trailer';
 
 describe('Trailer - Unit', () => {
   it('is a Vue instance', () => {
     const wrapper = shallowMount(Trailer)
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('Contem verificar se existe a classe videos - Unit', () => {
+    const wrapper = mount(Trailer)
+    expect(wrapper.classes('.videos'))
+  })
+
+  test('Contem src - Unit', () => {
+    const wrapper = mount(Trailer)
+    const src = wrapper.find('src')
+    expect(src).toBe(src)
+  })
+
+  test('verificando se existe caminho da imagem no src - Unit', () => {
+    const wrapper = mount(Trailer)
+    const src = wrapper.find('src')
+    src.exists(`https://www.youtube.com/embed/`)
+    expect(src.exists).toBeTruthy()
   })
 })
