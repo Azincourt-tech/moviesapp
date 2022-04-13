4caeee8e2e891c5f1197fdcd1b4147cb8009ea4a
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-04-13 17:04:01 -0300

    realizando teste unitario

--- test/Footer.spec.js
+++ test/Footer.spec.js
@@ -6,4 +6,23 @@ describe('Footer - Unit', () => {
     const wrapper = mount(Footer)
     expect(wrapper.vm).toBeTruthy()
   })
+
+  test('verificar se existe a classe copyright - Unit', () => {
+    const wrapper = mount(Footer)
+    expect(wrapper.classes('.copyright'))
+  })
+
+  test('verificar se existe a tag href - Unit', () => {
+    const wrapper = mount(Footer)
+    const href = wrapper.find('href')
+    expect(href).toBe(href)
+  })
+
+  test('verificando se existe link em href - Unit', () => {
+    const wrapper = mount(Footer)
+    const href = wrapper.find('href')
+    href.exists(`https://github.com/Azincourt-tech`)
+    expect(href.exists).toBeTruthy()
+  })
+  
 })
